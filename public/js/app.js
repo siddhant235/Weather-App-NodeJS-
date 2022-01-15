2d6aa231ac79c2efe0502929c0c277bd2c667bf8
siddhant235 <siddhant235@gmail.com>
2022-01-15 12:45:33 +0530

    styling weather page done

--- public/js/app.js
+++ public/js/app.js
@@ -4,16 +4,25 @@ console.log("Client side js file loaded")
 
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
+const messageOne=document.querySelector('#message-1')
+const messageTwo=document.querySelector('#message-2')
+
+// messageOne.textContent='From JavaScript'
+
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const location=search.value
     console.log(location)
+    messageOne.textContent='Loading...'
+    messageTwo.textContent=''
     fetch('http://localhost:4000/weather?address='+location).then((res)=>{
     res.json().then((data)=>{
+      
              if(data.error){
-   console.log(data.error)
+   messageOne.textContent=data.error
              }else{
-            console.log(data.forecast)
+           messageOne.textContent=data.location;
+           messageTwo.textContent="Its is currently "+data.forecast.temperature+" degrees and feels like "+data.forecast.feelsLike+" degrees"
              }
     })
 })
